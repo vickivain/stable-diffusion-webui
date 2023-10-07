ef3808a1f33532b0abe7161baa5dcb1352bd83a1
vickivain <blindmelon861@gmail.com>
2023-10-07 11:39:29 -0700

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,9 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--theme dark --no-half-vae --xformers --skip-python-version-check
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
+set ATTN_PRECISION=fp16
+set SAFETENSORS_FAST_GPU=1
 
 call webui.bat
