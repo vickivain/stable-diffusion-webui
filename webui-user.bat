f76d2545e6aca61844a962cda92d4d9c7e5ce915
vickivain <73156605+vickivain@users.noreply.github.com>
2024-02-13 17:31:29 -0800

    why not
    sure

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,10 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:64
+set COMMANDLINE_ARGS=--theme dark --no-half --no-half-vae --opt-sdp-no-mem-attention --xformers --skip-python-version-check
+set OPTIMIZED_TURBO=true
+set ATTN_PRECISION=fp16
+set SAFETENSORS_FAST_GPU=1
 
 call webui.bat
