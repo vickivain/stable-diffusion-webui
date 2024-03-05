ea4596abd4087eedf03a67d11fdc2a7f6cae7321
anapnoe <124302297+anapnoe@users.noreply.github.com>
2024-03-05 14:49:26 +0200

    remove COMMANDLINE_ARGS

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,5 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:512
-set COMMANDLINE_ARGS=--xformers --no-half-vae --medvram --medvram-sdxl --no-half 
+set COMMANDLINE_ARGS=
 call webui.bat
