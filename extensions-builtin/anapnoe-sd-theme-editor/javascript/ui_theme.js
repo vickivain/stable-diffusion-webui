ac7c54333be5ae2eea311d6659e03b269f596255
anapnoe <124302297+anapnoe@users.noreply.github.com>
2024-09-19 20:13:28 +0300

    Update ui_theme.js

--- extensions-builtin/anapnoe-sd-theme-editor/javascript/ui_theme.js
+++ extensions-builtin/anapnoe-sd-theme-editor/javascript/ui_theme.js
@@ -3,14 +3,14 @@ function hexToRgb(color) {
   let c;
 
   if (hex.length !== 6) {
-      hex = (() => {
-          const result = [];
-          for (let i = 0; i < Array.from(hex).length; i++) {
-              c = Array.from(hex)[i];
-              result.push(`${c}${c}`);
-          }
-          return result;
-      })().join("");
+    hex = (() => {
+      const result = [];
+      for (let i = 0; i < Array.from(hex).length; i++) {
+        c = Array.from(hex)[i];
+        result.push(`${c}${c}`);
+      }
+      return result;
+    })().join("");
   }
 
   const colorStr = hex.match(/#?(.{2})(.{2})(.{2})/).slice(1);
@@ -29,10 +29,10 @@ function rgbToHsl(rgb) {
   const add = max + min;
 
   const hue = min === max
-      ? 0
-      : r === max
-          ? (60 * (g - b) / diff + 360) % 360
-          : g === max ? 60 * (b - r) / diff + 120 : 60 * (r - g) / diff + 240;
+    ? 0
+    : r === max
+      ? (60 * (g - b) / diff + 360) % 360
+      : g === max ? 60 * (b - r) / diff + 120 : 60 * (r - g) / diff + 240;
 
   const lum = 0.5 * add;
   const sat = lum === 0 ? 0 : lum === 1 ? 1 : lum <= 0.5 ? diff / add : diff / (2 - add);
@@ -51,11 +51,11 @@ function hslToHex(h, s, l) {
   const a = s * Math.min(l, 1 - l) / 100;
 
   const f = (n) => {
-      const k = (n + h / 30) % 12;
-      const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-      return Math.round(255 * Math.max(0, Math.min(color, 1)))
-          .toString(16)
-          .padStart(2, "0");
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * Math.max(0, Math.min(color, 1)))
+      .toString(16)
+      .padStart(2, "0");
   };
 
   return `#${f(0)}${f(8)}${f(4)}`;
@@ -65,7 +65,7 @@ function hsl2rgb(h, s, l) {
   const a = s * Math.min(l, 1 - l);
 
   const f = (n = (n + h / 30) % 12) => {
-      return l - a * Math.max(Math.min(n - 3, 9 - n, 1), -1);
+    return l - a * Math.max(Math.min(n - 3, 9 - n, 1), -1);
   };
 
   return [f(0), f(8), f(4)];
@@ -73,15 +73,15 @@ function hsl2rgb(h, s, l) {
 
 function invertColor(hex) {
   if (hex.startsWith("#")) {
-      hex = hex.slice(1);
+    hex = hex.slice(1);
   }
 
   if (hex.length === 3) {
-      hex = hex.split("").map(c => c + c).join("");
+    hex = hex.split("").map(c => c + c).join("");
   }
 
   if (hex.length !== 6) {
-      throw new Error("Invalid HEX color.");
+    throw new Error("Invalid HEX color.");
   }
 
   const r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16);
@@ -111,54 +111,54 @@ function offsetColorsHSV(ohsl) {
   let inner_styles = "";
 
   for (const key in styleobj) {
-      const keyVal = styleobj[key];
+    const keyVal = styleobj[key];
 
-      if (keyVal.includes("#") || keyVal.includes("hsl")) {
-          const colcomp = document.body.querySelector(`#${key} input`);
+    if (keyVal.includes("#") || keyVal.includes("hsl")) {
+      const colcomp = document.body.querySelector(`#${key} input`);
 
-          if (colcomp) {
-              let hsl;
+      if (colcomp) {
+        let hsl;
 
-              if (keyVal.includes("#")) {
-                  keyVal = keyVal.replace(/\s+/g, "");
+        if (keyVal.includes("#")) {
+          keyVal = keyVal.replace(/\s+/g, "");
 
-                  if (isColorsInv) {
-                      keyVal = invertColor(keyVal);
-                      styleobj[key] = keyVal;
-                  }
-
-                  hsl = rgbToHsl(hexToRgb(keyVal));
-              } else {
-                  if (isColorsInv) {
-                      const c = toHSLArray(keyVal);
-                      const _hex = hslToHex(c[0], c[1], c[2]);
-                      keyVal = invertColor(_hex);
-                      styleobj[key] = keyVal;
-                      hsl = rgbToHsl(hexToRgb(keyVal));
-                  } else {
-                      hsl = toHSLArray(keyVal);
-                  }
-              }
-
-              const h = (parseInt(hsl[0]) + parseInt(ohsl[0])) % 360;
-              const s = Math.min(Math.max(parseInt(hsl[1]) + parseInt(ohsl[1]), 0), 100);
-              const l = Math.min(Math.max(parseInt(hsl[2]) + parseInt(ohsl[2]), 0), 100);
-
-              const hex = hslToHex(h, s, l);
-              colcomp.value = hex;
-              hslobj[key] = `hsl(${h}deg ${s}% ${l}%)`;
-              inner_styles += `${key}:${hslobj[key]};`;
+          if (isColorsInv) {
+            keyVal = invertColor(keyVal);
+            styleobj[key] = keyVal;
           }
-      } else {
-          inner_styles += `${key}:${styleobj[key]};`;
+
+          hsl = rgbToHsl(hexToRgb(keyVal));
+        } else {
+          if (isColorsInv) {
+            const c = toHSLArray(keyVal);
+            const _hex = hslToHex(c[0], c[1], c[2]);
+            keyVal = invertColor(_hex);
+            styleobj[key] = keyVal;
+            hsl = rgbToHsl(hexToRgb(keyVal));
+          } else {
+            hsl = toHSLArray(keyVal);
+          }
+        }
+
+        const h = (parseInt(hsl[0]) + parseInt(ohsl[0])) % 360;
+        const s = Math.min(Math.max(parseInt(hsl[1]) + parseInt(ohsl[1]), 0), 100);
+        const l = Math.min(Math.max(parseInt(hsl[2]) + parseInt(ohsl[2]), 0), 100);
+
+        const hex = hslToHex(h, s, l);
+        colcomp.value = hex;
+        hslobj[key] = `hsl(${h}deg ${s}% ${l}%)`;
+        inner_styles += `${key}:${hslobj[key]};`;
       }
+    } else {
+      inner_styles += `${key}:${styleobj[key]};`;
+    }
   }
 
   isColorsInv = false;
   const preview_styles = document.body.querySelector("#preview-styles");
   preview_styles.innerHTML = `:root {${inner_styles}}`;
   preview_styles.innerHTML +=
-      `@media only screen and (max-width: 860px) {:root{--ae-outside-gap-size: var(--ae-mobile-outside-gap-size);--ae-inside-padding-size: var(--ae-mobile-inside-padding-size);}}`;
+    `@media only screen and (max-width: 860px) {:root{--ae-outside-gap-size: var(--ae-mobile-outside-gap-size);--ae-inside-padding-size: var(--ae-mobile-inside-padding-size);}}`;
   const vars_textarea = document.body.querySelector("#theme_vars textarea");
   vars_textarea.value = inner_styles;
   window.updateInput(vars_textarea);
@@ -168,33 +168,33 @@ function updateTheme(vars) {
   let inner_styles = "";
 
   for (let i = 0; i < vars.length - 1; i++) {
-      const [id, val] = vars[i].split(":").map(v => v.trim());
-      styleobj[id.replace(/\s+/g, "")] = val;
-      inner_styles += `${id}:${val};`;
-      const elem = document.body.querySelectorAll(`#${id} input`);
-      elem.forEach((el) => {
-          if (val.includes("hsl")) {
-              const hsl = toHSLArray(val);
-              el.value = hslToHex(hsl[0], hsl[1], hsl[2]);
-          } else {
-              el.value = val.split("px")[0];
-          }
-      });
+    const [id, val] = vars[i].split(":").map(v => v.trim());
+    styleobj[id.replace(/\s+/g, "")] = val;
+    inner_styles += `${id}:${val};`;
+    const elem = document.body.querySelectorAll(`#${id} input`);
+    elem.forEach((el) => {
+      if (val.includes("hsl")) {
+        const hsl = toHSLArray(val);
+        el.value = hslToHex(hsl[0], hsl[1], hsl[2]);
+      } else {
+        el.value = val.split("px")[0];
+      }
+    });
   }
 
   const preview_styles = document.body.querySelector("#preview-styles");
 
   if (preview_styles) {
-      preview_styles.innerHTML = `:root {${inner_styles}}`;
-      preview_styles.innerHTML +=
-          `@media only screen and (max-width: 860px) {:root{--ae-outside-gap-size: var(--ae-mobile-outside-gap-size);--ae-inside-padding-size: var(--ae-mobile-inside-padding-size);}}`;
+    preview_styles.innerHTML = `:root {${inner_styles}}`;
+    preview_styles.innerHTML +=
+      `@media only screen and (max-width: 860px) {:root{--ae-outside-gap-size: var(--ae-mobile-outside-gap-size);--ae-inside-padding-size: var(--ae-mobile-inside-padding-size);}}`;
   } else {
-      const style = document.createElement("style");
-      style.id = "preview-styles";
-      style.innerHTML = `:root {${inner_styles}}`;
-      style.innerHTML +=
-          `@media only screen and (max-width: 860px) {:root{--ae-outside-gap-size: var(--ae-mobile-outside-gap-size);--ae-inside-padding-size: var(--ae-mobile-inside-padding-size);}}`;
-      document.body.appendChild(style);
+    const style = document.createElement("style");
+    style.id = "preview-styles";
+    style.innerHTML = `:root {${inner_styles}}`;
+    style.innerHTML +=
+      `@media only screen and (max-width: 860px) {:root{--ae-outside-gap-size: var(--ae-mobile-outside-gap-size);--ae-inside-padding-size: var(--ae-mobile-inside-padding-size);}}`;
+    document.body.appendChild(style);
   }
 
   const vars_textarea = document.body.querySelector("#theme_vars textarea");
@@ -218,141 +218,141 @@ function initTheme(styles) {
   vars_textarea.value = init_css_vars;
   const additional_styles = css_styles[1] !== undefined ? css_styles[1] : "";
   css_textarea.value =
-      `/*BREAKPOINT_CSS_CONTENT*/${additional_styles}/*BREAKPOINT_CSS_CONTENT*/`;
+    `/*BREAKPOINT_CSS_CONTENT*/${additional_styles}/*BREAKPOINT_CSS_CONTENT*/`;
   updateTheme(init_vars);
 
   const preview_styles = document.body.querySelector("#preview-styles");
   if (!preview_styles) {
-      const style = document.createElement("style");
-      style.id = "preview-styles";
-      style.innerHTML = styles;
-      document.body.appendChild(style);
+    const style = document.createElement("style");
+    style.id = "preview-styles";
+    style.innerHTML = styles;
+    document.body.appendChild(style);
   }
 
   let intervalChange;
   document.body
-      .querySelectorAll("#ui_theme_settings input")
-      .forEach((elem) => {
-          elem.addEventListener("input", (e) => {
-              let val = e.currentTarget.value;
-              if (e.currentTarget.type === "range" || e.currentTarget.type === "number") {
-                  val += "px";
-              } 
-              if (e.currentTarget.type === "color") {
-                  val = e.currentTarget.value;
-              }
+    .querySelectorAll("#ui_theme_settings input")
+    .forEach((elem) => {
+      elem.addEventListener("input", (e) => {
+        let val = e.currentTarget.value;
+        if (e.currentTarget.type === "range" || e.currentTarget.type === "number") {
+          val += "px";
+        }
+        if (e.currentTarget.type === "color") {
+          val = e.currentTarget.value;
+        }
 
-              styleobj[e.currentTarget.parentElement.id] = val;
+        styleobj[e.currentTarget.parentElement.id] = val;
 
-              if (intervalChange != null) clearInterval(intervalChange);
-              intervalChange = setTimeout(() => {
-                  let inner_styles = "";
-                  for (const key in styleobj) {
-                      inner_styles += `${key}:${styleobj[key]};`;
-                  }
+        if (intervalChange != null) clearInterval(intervalChange);
+        intervalChange = setTimeout(() => {
+          let inner_styles = "";
+          for (const key in styleobj) {
+            inner_styles += `${key}:${styleobj[key]};`;
+          }
 
-                  vars_textarea.value = inner_styles;
-                  preview_styles.innerHTML = `:root {${inner_styles}}`;
-                  preview_styles.innerHTML +=
-                      `@media only screen and (max-width: 860px) {:root{--ae-outside-gap-size: var(--ae-mobile-outside-gap-size);--ae-inside-padding-size: var(--ae-mobile-inside-padding-size);}}`;
+          vars_textarea.value = inner_styles;
+          preview_styles.innerHTML = `:root {${inner_styles}}`;
+          preview_styles.innerHTML +=
+            `@media only screen and (max-width: 860px) {:root{--ae-outside-gap-size: var(--ae-mobile-outside-gap-size);--ae-inside-padding-size: var(--ae-mobile-inside-padding-size);}}`;
 
-                  window.updateInput(vars_textarea);
-                  offsetColorsHSV(hsloffset);
-              }, 1000);
-          });
+          window.updateInput(vars_textarea);
+          offsetColorsHSV(hsloffset);
+        }, 1000);
       });
+    });
 
   const reset_btn = document.getElementById("theme_reset_btn");
   reset_btn.addEventListener("click", (e) => {
-      e.preventDefault();
-      document.body
-          .querySelectorAll("#ui_theme_hsv input")
-          .forEach((elem) => {
-              elem.value = 0;
-          });
-      hsloffset = [0, 0, 0];
-      updateTheme(init_vars);
+    e.preventDefault();
+    document.body
+      .querySelectorAll("#ui_theme_hsv input")
+      .forEach((elem) => {
+        elem.value = 0;
+      });
+    hsloffset = [0, 0, 0];
+    updateTheme(init_vars);
   });
 
   let intervalCheck;
 
   function dropDownOnChange() {
-      if (init_css_vars !== vars_textarea.value) {
-          clearInterval(intervalCheck);
-          init_css_vars = vars_textarea.value.replace(/\n|\r/g, "");
-          vars_textarea.value = init_css_vars;
-          const vars = init_css_vars.split(";");
-          updateTheme(vars);
-      }
+    if (init_css_vars !== vars_textarea.value) {
+      clearInterval(intervalCheck);
+      init_css_vars = vars_textarea.value.replace(/\n|\r/g, "");
+      vars_textarea.value = init_css_vars;
+      const vars = init_css_vars.split(";");
+      updateTheme(vars);
+    }
   }
 
   const drop_down = document.body.querySelector("#themes_drop_down input");
   drop_down.addEventListener("click", (e) => {
-      if (intervalCheck !== null) clearInterval(intervalCheck);
-      vars_textarea.value = init_css_vars;
-      intervalCheck = setInterval(dropDownOnChange, 500);
+    if (intervalCheck !== null) clearInterval(intervalCheck);
+    vars_textarea.value = init_css_vars;
+    intervalCheck = setInterval(dropDownOnChange, 500);
   });
 
   let hsloffset = [0, 0, 0];
 
   document.body
-      .querySelectorAll("#theme_hue input")
-      .forEach((elem) => {
-          elem.addEventListener("change", (e) => {
-              e.preventDefault();
-              hsloffset[0] = e.currentTarget.value;
-              offsetColorsHSV(hsloffset);
-          });
+    .querySelectorAll("#theme_hue input")
+    .forEach((elem) => {
+      elem.addEventListener("change", (e) => {
+        e.preventDefault();
+        hsloffset[0] = e.currentTarget.value;
+        offsetColorsHSV(hsloffset);
       });
+    });
 
   document.body
-      .querySelectorAll("#theme_sat input")
-      .forEach((elem) => {
-          elem.addEventListener("change", (e) => {
-              e.preventDefault();
-              hsloffset[1] = e.currentTarget.value;
-              offsetColorsHSV(hsloffset);
-          });
+    .querySelectorAll("#theme_sat input")
+    .forEach((elem) => {
+      elem.addEventListener("change", (e) => {
+        e.preventDefault();
+        hsloffset[1] = e.currentTarget.value;
+        offsetColorsHSV(hsloffset);
       });
+    });
 
   document.body
-      .querySelectorAll("#theme_brt input")
-      .forEach((elem) => {
-          elem.addEventListener("change", (e) => {
-              e.preventDefault();
-              hsloffset[2] = e.currentTarget.value;
-              offsetColorsHSV(hsloffset);
-          });
+    .querySelectorAll("#theme_brt input")
+    .forEach((elem) => {
+      elem.addEventListener("change", (e) => {
+        e.preventDefault();
+        hsloffset[2] = e.currentTarget.value;
+        offsetColorsHSV(hsloffset);
       });
+    });
 
   const inv_btn = document.getElementById("theme_invert_btn");
   inv_btn.addEventListener("click", (e) => {
-      e.preventDefault();
-      isColorsInv = !isColorsInv;
-      offsetColorsHSV(hsloffset);
+    e.preventDefault();
+    isColorsInv = !isColorsInv;
+    offsetColorsHSV(hsloffset);
   });
 }
 
 function observeGradioApp() {
   const observer = new MutationObserver(() => {
-      const css = document.querySelector('[rel="stylesheet"][href*="user"]');
-      const block = gradioApp().getElementById("tab_ui_theme");
+    const css = document.querySelector('[rel="stylesheet"][href*="user"]');
+    const block = gradioApp().getElementById("tab_ui_theme");
 
-      if (css && block) {
-          observer.disconnect();
-          setTimeout(() => {
-              const rootRules = Array.from(css.sheet.cssRules).filter((cssRule) => {
-                  return cssRule instanceof CSSStyleRule && cssRule.selectorText === ":root";
-              });
-              const rootCssText = rootRules[0].cssText;
-              initTheme(rootCssText);
-          }, 500);
-      }
+    if (css && block) {
+      observer.disconnect();
+      setTimeout(() => {
+        const rootRules = Array.from(css.sheet.cssRules).filter((cssRule) => {
+          return cssRule instanceof CSSStyleRule && cssRule.selectorText === ":root";
+        });
+        const rootCssText = rootRules[0].cssText;
+        initTheme(rootCssText);
+      }, 500);
+    }
   });
 
   observer.observe(gradioApp(), {
-      childList: true,
-      subtree: true
+    childList: true,
+    subtree: true
   });
 }
 
