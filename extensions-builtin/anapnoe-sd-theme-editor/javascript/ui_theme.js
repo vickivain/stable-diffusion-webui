f3542d1d2ec541a634cc44de05b17b95e36e2014
anapnoe <124302297+anapnoe@users.noreply.github.com>
2024-09-19 21:54:04 +0300

    Update ui_theme.js

--- extensions-builtin/anapnoe-sd-theme-editor/javascript/ui_theme.js
+++ extensions-builtin/anapnoe-sd-theme-editor/javascript/ui_theme.js
@@ -28,21 +28,21 @@ function rgbToHsl(rgb) {
     const diff = max - min;
     const add = max + min;
 
-    const hue = min === max
-        ? 0
-        : r === max
-            ? (60 * (g - b) / diff + 360) % 360
-            : g === max
-                ? 60 * (b - r) / diff + 120
+    const hue = min === max 
+        ? 0 
+        : r === max 
+            ? (60 * (g - b) / diff + 360) % 360 
+            : g === max 
+                ? 60 * (b - r) / diff + 120 
                 : 60 * (r - g) / diff + 240;
 
     const lum = 0.5 * add;
-    const sat = lum === 0
-        ? 0
-        : lum === 1
-            ? 1
-            : lum <= 0.5
-                ? diff / add
+    const sat = lum === 0 
+        ? 0 
+        : lum === 1 
+            ? 1 
+            : lum <= 0.5 
+                ? diff / add 
                 : diff / (2 - add);
 
     return [
