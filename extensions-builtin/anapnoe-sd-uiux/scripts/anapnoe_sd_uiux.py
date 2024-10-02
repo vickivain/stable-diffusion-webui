7b3c03051203df1a5df8202f4c06345c51e513cf
anapnoe <124302297+anapnoe@users.noreply.github.com>
2024-10-02 23:31:39 +0300

    Split Anapnoe UI-UX options

--- extensions-builtin/anapnoe-sd-uiux/scripts/anapnoe_sd_uiux.py
+++ extensions-builtin/anapnoe-sd-uiux/scripts/anapnoe_sd_uiux.py
@@ -1,19 +1,17 @@
 import gradio as gr
 from modules import script_callbacks, shared
-from modules.options import options_section
+from modules.options import options_section, categories
 
 mapping = [
     (info.infotext, k) for k, info in shared.opts.data_labels.items() if info.infotext
 ]
 
+categories.register_category("anapnoe", "Anapnoe UI-UX", position=0)
+
 shared.options_templates.update(
     options_section(
-        ("uiux_core", "Anapnoe UI-UX", "anapnoe"),
+        ("anapnoe-core", "User Interface", "anapnoe"),
         {
-            "uiux_enable_console_log": shared.OptionInfo(False, "Enable console log"),
-            "uiux_max_resolution_output": shared.OptionInfo(
-                2048, "Max resolution output for txt2img and img2img"
-            ),
             "uiux_show_input_range_ticks": shared.OptionInfo(
                 True, "Show ticks for input range slider"
             ),
@@ -39,6 +37,17 @@ shared.options_templates.update(
             ),
             "uiux_hide_extra_info": shared.OptionInfo(
                 False, "Hide extra info for labels, checkboxes"
+            )
+        },
+    )
+)
+shared.options_templates.update(
+    options_section(
+        ("anapnoe-ext", "Utils Extensions", "anapnoe"),
+        {
+            "uiux_enable_console_log": shared.OptionInfo(False, "Enable console log"),
+            "uiux_max_resolution_output": shared.OptionInfo(
+                2048, "Max resolution output for txt2img and img2img"
             ),
             "uiux_ignore_overrides": shared.OptionInfo(
                 [],
